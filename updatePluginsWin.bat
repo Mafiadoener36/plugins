53e45d8f3f8680b394aff46d5f8ab036501d3c89
user0 <114065447+Mafiadoener36@users.noreply.github.com>
2025-03-14 19:15:27 +0100

    Create updatePluginsWin.bat

---
+++ updatePluginsWin.bat
@@ -0,0 +1,18 @@
+@echo off
+git clone https://gitlab.futo.org/videostreaming/plugins/apple-podcasts
+git clone https://gitlab.futo.org/videostreaming/plugins/bilibili
+git clone https://gitlab.futo.org/videostreaming/plugins/bitchute
+git clone https://gitlab.futo.org/videostreaming/plugins/dailymotion
+git clone https://gitlab.futo.org/videostreaming/plugins/kick
+git clone https://gitlab.futo.org/videostreaming/plugins/nebula
+git clone https://gitlab.futo.org/videostreaming/plugins/niconico
+git clone https://gitlab.futo.org/videostreaming/plugins/odysee
+git clone https://gitlab.futo.org/videostreaming/plugins/patreon
+git clone https://gitlab.futo.org/videostreaming/plugins/peertube
+git clone https://gitlab.futo.org/videostreaming/plugins/rumble
+git clone https://gitlab.futo.org/videostreaming/plugins/sample
+git clone https://gitlab.futo.org/videostreaming/plugins/soundcloud
+git clone https://gitlab.futo.org/videostreaming/plugins/spotify
+git clone https://gitlab.futo.org/videostreaming/plugins/twitch
+git clone https://gitlab.futo.org/videostreaming/plugins/youtube
+exit
